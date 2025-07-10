bbbe75d4020073717036a6f78ebe581045dc7215
vematu <ematuv99@gmail.com>
2025-07-10 05:11:57 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "gate-fee-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./home.html",
+  "./login.html",
+  "./add-user.html",
+  "./enter-fee.html",
+  "./view-data.html",
+  "./manifest.json",
+  // add your CSS, JS, icons here as needed
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
