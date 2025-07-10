55b0fb8926322fd00e29cfbfd5141c145a8f3243
vematu <ematuv99@gmail.com>
2025-07-10 09:35:03 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,26 +2,27 @@ const CACHE_NAME = "gate-fee-cache-v1";
 const urlsToCache = [
   "./",
   "./index.html",
-  "./home.html",
   "./login.html",
+  "./home.html",
   "./add-user.html",
   "./enter-fee.html",
   "./view-data.html",
   "./manifest.json",
-  // add your CSS, JS, icons here as needed
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
     })
   );
